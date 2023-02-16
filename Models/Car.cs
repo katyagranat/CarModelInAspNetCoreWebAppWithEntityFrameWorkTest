7ce63b9e9cc2d1545f5f7caf0ccc139f95a0d9dd
GranatKatya <k.granat2017@gmail.com>
2023-02-16 16:10:43 +0200

    first commit

---
+++ Models/Car.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarModelInAspNetCoreWebAppWithEntityFrameWorkTest.Models;
+
+public class Car
+{
+    public int Id { get; set; }
+    public string Producer { get; set; }
+    public string Model { get; set; }
+    public double EnginePower { get; set; }
+    public string EngineType { get; set; }
+    public int NumberOfCylinders { get; set; }
+    public string Color { get; set; }
+    public DateTime DateOfCreating { get; set; }
+    public bool Availability { get; set; }
+    public byte[]? Photo { get; set; }
+
+   // public IFormFile Photo { get; set; }
+
+    // [Required]
+    //[Display(Name = "Photo")]
+    // public IFormFile Photo { get; set; }
+
+
+    //[DataType(DataType.Date)]
+    //public DateTime ReleaseDate { get; set; }
+}
